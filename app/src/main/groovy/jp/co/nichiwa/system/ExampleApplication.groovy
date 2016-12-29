c1cc3cfb743433d24d7ae5a382e5dba4069beebd
Y Okumura <y-okumura@nichiwa-system.co.jp>
2016-12-29 23:08:25 +0900

    サンプルアプリをstart.spring.ioで作成
    Web - MyBATIS - MySQL。
    Datasourceの設定の問題で起動はできない

---
+++ app/src/main/groovy/jp/co/nichiwa/system/ExampleApplication.groovy
@@ -0,0 +1,12 @@
+package jp.co.nichiwa.system
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ExampleApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run ExampleApplication, args
+	}
+}

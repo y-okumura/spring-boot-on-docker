83efedc44b35d6b34c2a09959e32f500c18086ea
Y Okumura <y-okumura@nichiwa-system.co.jp>
2017-01-01 10:39:18 +0900

    少し整理

--- app/src/main/groovy/jp/co/nichiwa_system/example/ExampleMapper.groovy
+++ app/src/main/groovy/jp/co/nichiwa_system/example/ExampleMapper.groovy
@@ -1,7 +1,6 @@
 package jp.co.nichiwa_system.example
 
 import org.apache.ibatis.annotations.Mapper
-import org.apache.ibatis.annotations.Options
 import org.apache.ibatis.annotations.Select
 
 @Mapper
--- app/src/main/groovy/jp/co/nichiwa_system/example/HelloController.groovy
+++ app/src/main/groovy/jp/co/nichiwa_system/example/HelloController.groovy
@@ -1,6 +1,6 @@
 package jp.co.nichiwa_system.example
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
